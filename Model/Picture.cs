b0b6fd72d33a2a021edc0a35dfbfb3edbf4efbe8
ranerijosh <43115830+ranerijosh@users.noreply.github.com>
2021-02-10 11:35:34 -0500

    Creation

---
+++ Model/Picture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Picture
+    {
+        [Key]
+        [DisplayName("Picture ID")]
+        public Guid PictureID { get; set; }
+        [DisplayName("Submitted By")]
+        public string SubmitterID { get; set; }
+        [DisplayName("Image String")]
+        public string ImageString { get; set; }
+        [DisplayName("Image Array")]
+        public byte[] ImageArray { get; set; }
+    }
+}
---
+++ Repository/AlbumContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AlbumContext : DbContext
+    {
+        public DbSet<Picture> Pictures;
+        public DbSet<LeagueArticle> LeagueArticles;
+
+        public NewsContext() { }
+
+        public NewsContext(DbContextOptions<NewsContext> options) : base(options) { }
+    }
+}
